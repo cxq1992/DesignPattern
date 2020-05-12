d438fb16c9efffcf315173be5a53b680d7cb838c
Kyle Chen <554019751@qq.com>
2020-05-12 23:05:40 +0800

    add singleton

---
+++ cpp/creational/singleton.h
@@ -0,0 +1,23 @@
+#ifndef SINGLETON_H
+#define SINGLETON_H
+namespace design_pattern {
+namespace creational {
+class President
+{
+public:
+    President& CreatePresident()
+    {
+        static President president = President();
+        return president;
+    }
+private:
+    President() = default;
+    President(const President &) = delete;
+    President(President &&) = delete;
+    President & operator=(const President&) = delete;
+    President & operator=(President&&) = delete;
+
+};
+}
+}
+#endif // SINGLETON_H
