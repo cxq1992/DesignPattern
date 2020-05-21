25a99ec7fd8dd4640805cda66ee26b57f457c66b
Kyle Chen <554019751@qq.com>
2020-05-21 23:27:23 +0800

    add facade

---
+++ cpp/structural/facade.hpp
@@ -0,0 +1,81 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <memory>
+#include <cassert>
+
+namespace designpattern{
+namespace structual{
+class Computer{
+    public:
+        void GetElectricShock()
+        {
+            std::cout << "Ouch!" << std::endl;
+        }
+
+        void MakeSound()
+        {
+            std::cout << "Beep beep!" << std::endl;
+        }
+
+        void Start()
+        {
+            std::cout << "Starting..." << std::endl;
+        }
+
+        void ShowLoadingScreen()
+        {
+            std::cout << "Loading..." << std::endl;
+        }
+
+        void Ready()
+        {
+            std::cout << "Ready to be used!" << std::endl;
+        }
+
+        void Shutdown()
+        {
+            std::cout << "Shutting down..." << std::endl;
+        }
+
+        void PullCurrent()
+        {
+            std::cout << "Current pulled off" << std::endl;
+        }
+
+        void Sleep()
+        {
+            std::cout << "Zzzz" << std::endl;
+        }
+};
+
+class ComputerFacade
+{
+    public:
+    explicit
+    ComputerFacade(std::shared_ptr<Computer> computer)
+        : _computer(computer)
+    {
+        
+    }
+    void Turnon()
+    {
+        assert(_computer != nullptr);
+        _computer->GetElectricShock();
+        _computer->MakeSound();
+        _computer->Start();
+        _computer->ShowLoadingScreen();
+        _computer->Ready();
+    }
+    void Turnoff()
+    {
+        assert(_computer != nullptr);
+        _computer->Shutdown();
+        _computer->PullCurrent();
+    }
+    private:
+    std::shared_ptr<Computer> _computer;
+};
+
+}
+} // namespace designpattern
